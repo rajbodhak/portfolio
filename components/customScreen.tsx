8dddacc2a0853e90882450432294f739f5e99b67
Raj Bodhak <rajlaptop2004@gmail.com>
2025-06-08 21:16:20 +0530

    implementing custom screen

--- components/customScreen.tsx
+++ components/customScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const customScreen = () => {
     return (
-        <div>
+        <div className='md:w-[865px] mx-auto text-gray-700 dark:text-white md:mt-10 mt-7 pb-24 font-popins px-2 md:px-0'>
 
         </div>
     )
