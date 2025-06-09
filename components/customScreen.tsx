74e0c2aca82c7abab8babffbb1e1b5401acfe848
Raj Bodhak <rajlaptop2004@gmail.com>
2025-06-09 23:11:07 +0530

    Update custom Screen

--- components/customScreen.tsx
+++ components/customScreen.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import { motion } from "framer-motion"
+"use client"
+import React from 'react';
+import { motion } from "framer-motion";
 
-const customScreen = () => {
+interface CustomScreenProps {
+    children: React.ReactNode
+}
+
+const CustomScreen = ({ children }: CustomScreenProps) => {
     return (
         <div className='md:w-[865px] mx-auto text-gray-700 dark:text-white md:mt-10 mt-7 pb-24 font-popins px-2 md:px-0'>
-
+            <motion.div
+                initial={{ opacity: 0, y: 20, filter: "blur(5px)" }}
+                animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+                transition={{ duration: 0.8 }}
+            >
+                {children}
+            </motion.div>
         </div>
     )
 }
 
-export default customScreen
+export default CustomScreen;
