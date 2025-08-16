188ebc64520e834501f96499bcb3d4793d460312
Raj Bodhak <rajlaptop2004@gmail.com>
2025-08-16 13:29:00 +0530

    update import style

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import AboutMe from "@/components/AboutMe";
-import CustomScreen from "@/components/CustomScreen";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Intro from "@/components/Intro";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+import AboutMe from "../components/AboutMe";
+import CustomScreen from "../components/CustomScreen";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Intro from "../components/Intro";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
