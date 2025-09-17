99120fd1d798a8a8d5ff6beac9b54c18f4e26829
Raj Bodhak <rajlaptop2004@gmail.com>
2025-09-17 13:04:24 +0530

    Update intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -32,29 +32,30 @@ const Intro = () => {
     return (
 
         <div className='flex flex-row gap-3 items-center'>
-            <div className='relative size-28 md:size-36 border border-[#FF6B6B] rounded-md overflow-hidden'>
+            <div className='relative size-28 md:size-36 bg-bg-primary rounded-2xl p-1'
+                style={{ boxShadow: 'var(--shadow-3d-border-lg)' }}>
                 <Image src="/rajidesu.webp"
                     alt='rajidesu'
                     fill
-                    className='object-cover '
+                    className='object-cover rounded-xl'
                     sizes="(max-width:768px) 128px, 160px"
                     priority
                 />
             </div>
             <div className='flex flex-col'>
                 <div className='text-2xl md:text-4xl font-bold capitalized'>
-                    <span className='bg-gradient-to-br from-[#8B0000] via-[#C9184A] to-[#FF4E50] text-transparent bg-clip-text'>Raj Bodhak</span>
+                    <span className='text-primary-custom bg-clip-text'>Raj Bodhak</span>
                 </div>
                 <div className='text-sm'>
-                    <span className="text-[#E0E0E0]">Full Stack Developer</span>
+                    <span className="text-secondary-custom">Full Stack Developer</span>
                 </div>
-                <div className='flex flex-wrap gap-2 items-center text-lg md:text-xl mt-2 text-[#FF6B6B] '>
+                <div className='flex flex-wrap gap-2 items-center text-lg md:text-xl mt-2 text-primary-custom '>
                     {links.map((item) => (
                         <Link
                             key={item.name}
                             href={item.link}
                             target='_blank'
-                            className='border p-1 rounded-sm hover:text-[#fa9090]'
+                            className='border p-1 rounded-sm hover:text-secondary-custom'
                         >
                             {item.logo}
                         </Link>
