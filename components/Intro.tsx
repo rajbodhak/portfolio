60af4ae835d2169a72ed1a7c7d7e126a8e132a7f
Raj Bodhak <rajlaptop2004@gmail.com>
2025-09-18 12:29:09 +0530

    Added Gmail Link

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -25,7 +25,7 @@ const Intro = () => {
         },
         {
             name: "gmail",
-            link: "",
+            link: "mailto:rajlaptop2004@gmail.com",
             logo: <BiLogoGmail />
         }
     ]
